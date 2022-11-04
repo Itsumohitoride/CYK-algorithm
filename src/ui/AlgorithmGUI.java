7886ffc1f1e7ea4f1c9fc1be79541f18dab85e3f
Luis Miguel Ossa Arias <luismiguelossaarias05@gmail.com>
2022-11-04 16:53:19 -0500

    The mainPain was added

---
+++ src/ui/AlgorithmGUI.java
@@ -0,0 +1,54 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import model.AlgorithmCYK;
+
+public class AlgorithmGUI {
+	
+	public AlgorithmCYK algorithmCYK;
+	
+	@FXML
+	private BorderPane mainPane;
+	
+	@FXML
+	private Pane insertValues;
+	
+	@FXML
+    private TextField contentState;
+
+    @FXML
+    private TextField state;
+	
+	public AlgorithmGUI(AlgorithmCYK algorithmCYK) {
+		this.algorithmCYK = algorithmCYK;
+	}
+	
+	@FXML
+    public void newState(ActionEvent event) {
+		TextField newState = new TextField();
+		newState.setPrefHeight(state.getPrefHeight());
+		newState.setPrefWidth(state.getPrefWidth());
+		newState.setLayoutX(state.getLayoutX());
+		newState.setLayoutY(state.getLayoutY() + 20);
+		insertValues.getChildren().add(newState);
+    }
+
+	public void loadBanner() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("startProgram.fxml"));
+
+		loader.setController(this);
+		Parent load = loader.load();
+
+		mainPane.getChildren().clear();
+		mainPane.setTop(load);
+	}
+
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,42 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.AlgorithmCYK;
 
+public class Main extends Application{
+	
+	private AlgorithmGUI algorithmGUI;
+	private AlgorithmCYK algorithmCYK;
+	
+	public Main(){
+		algorithmCYK = new AlgorithmCYK();
+	}
+	
 	public static void main(String[] args) {
-
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		algorithmGUI = new AlgorithmGUI(algorithmCYK);
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-pane.fxml"));
+		
+		fxmlLoader.setController(algorithmGUI);
+		
+		Parent root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Autómatas de Estados Finitos");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		algorithmGUI.loadBanner();
 	}
 
 }
