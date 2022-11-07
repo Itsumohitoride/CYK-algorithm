b3ae54ff4cf148217477a27ff081d7f045b31998
Santiagokmids <sntgtrochez@gmail.com>
2022-11-06 20:53:42 -0500

    Methods of add parameters have been added

--- src/model/AlgorithmCYK.java
+++ src/model/AlgorithmCYK.java
@@ -10,8 +10,38 @@ public class AlgorithmCYK {
 	String string;
 	int sizeString;
 	
-	public AlgorithmCYK() {}
+	public AlgorithmCYK() {
+		states = new Hashtable<>();
+		numberStates = 0;
+		sizeString = 0;
+	}
 	
+	public ArrayList<String> splitProductions(String text){
+		String [] splitProductions = text.split("|");
+		
+		ArrayList<String> productionsOfState = new ArrayList<>();
+		
+		for (int i = 0; i < splitProductions.length; i++) {
+			productionsOfState.add(splitProductions[i]);
+		}
+		
+		return productionsOfState;
+	}
+	
+	public void addStates(String state, ArrayList<String> productions) {
+		states.put(state, productions);
+	}
+	
+	public void addString(String string) {
+		this.string = string;
+		sizeString = this.string.length();
+		System.out.println("tamaño del string "+sizeString);
+	}
+	
+	public boolean calculateCYK() {
+		
+		return false;
+	}
 	
 	
 }
